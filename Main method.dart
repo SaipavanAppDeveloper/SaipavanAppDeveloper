ec43e83e3ae850b53ab51eef9b0d748976721de9
saipavan <j.saipavan@thesmartq.com>
2023-07-06 13:01:45 +0530

    9th commit

--- Main method.dart
+++ Main method.dart
@@ -1,4 +1,6 @@
 //to run every dart program main method is the entry 
 void main(){
+  // For every end of the line we include semi column;
   print('hello world');
+  print('Im learning Dart ');
 }
